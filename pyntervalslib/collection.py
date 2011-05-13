004228423f2b054d5405af0a0907a13010033dc5
mpetit <michel.petit@af83.com>
2011-05-13 17:42:47 +0200

    Work in progress

--- pyntervalslib/collection.py
+++ pyntervalslib/collection.py
@@ -9,6 +9,17 @@ class Collection:
     '''
     actions = { 'time':['activeonly', 'approved', 'billable', 'clientid', 'date', 'daterange', 'list', 'milestoneid', 'moduleid', 'personid', 'projectid', 'taskid', 'worktypeid' 'sortfield', 'sortdir', 'offset', 'limit']}
 
+    allowed_fields = {
+            'time': ["id", "projectid", "moduleid", "taskid", "worktypeid", "personid", "date", "time", "description", "billable", "datemodified", "dateiso", "module", "project", "worktype", "tasklocalid", "task", "firstname", "lastname", "active", "person", "client", "clientactive", "statusid"],
+            "person": ["id", "clientid", "title", "firstname", "lastname", "primaryaccount", "notes", "allprojects", "active", "private", "username", "groupid", "group", "client", "numlogins", "lastlogin", "restricttasks"],
+            "client": ["id", "name", "datecreated", "description", "website", "phone", "fax", "address", "active"],
+            "customer": ["id", "billingid", "subdomain", "firstname", "lastname", "company", "email", "phone", "datesignup", "locale", "message", "plan", "domain", "timezone", "timezone_offset"],
+            "group": ["id", "name"],
+            "module":["id", "name", "description", "active"],
+            "project": ["id", "name", "description", "datestart", "dateend", "alert_percent", "alert_date", "active", "billable", "budget", "clientid", "client", "localid", "manager", "managerid"],
+            "task": ["id", "localid", "queueid", "assigneeid", "statusid", "projectid", "moduleid", "title", "summary", "dateopen", "datedue", "dateclosed", "estimate", "ownerid", "priorityid", "color", "priority", "severity", "status", "clientid", "client", "milestoneid", "milestone", "module", "assignees", "followers", "followerid", "owners"],
+            "worktype": ["id", "name", "defaulthourlyrate", "active"]
+            }
 
     def __init__(self, action):
         '''
